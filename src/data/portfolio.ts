ed9e6e7a4df9b6198ea8390a0aeb494a2361d4f6
deflillusion <defl.illusion@gmail.com>
2025-09-11 01:35:44 +0500

    fix info

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -5,34 +5,83 @@ export const portfolioProjects: PortfolioProject[] = [
         id: 1,
         title: "СОВРЕМЕННАЯ КВАРТИРА",
         category: "ЖИЛОЙ ИНТЕРЬЕР",
-        area: "85 М²",
-        image: "/portfolio/project-1/1-1.jpg",
+        area: "90 М²",
+        image: "/portfolio/project-1/img487.jpg",
         year: "2024",
         description: "Современная квартира в стиле минимализм с акцентом на функциональность и эстетику",
         tags: ["минимализм", "современный стиль", "функциональность"],
         featured: true,
         gallery: [
-            "/portfolio/project-1/1-1.jpg",
-            "/portfolio/project-1/1-2.jpg",
-            "/portfolio/project-1/1-3.jpg"
+            "/portfolio/project-1/img487.jpg",
+            "/portfolio/project-1/img479.jpg",
+            "/portfolio/project-1/img493.jpg",
+            "/portfolio/project-1/img500.jpg",
+            "/portfolio/project-1/img506.jpg",
+            "/portfolio/project-1/img512.jpg",
+            "/portfolio/project-1/img518.jpg"
         ]
     },
     {
         id: 2,
-        title: "СПАЛЬНЯ",
+        title: "СОВРЕМЕННАЯ КВАРТИРА",
         category: "ЖИЛОЙ ИНТЕРЬЕР",
-        area: "20 М²",
-        image: "/portfolio/project-2/spalnya-120-1.jpg",
+        area: "120 М²",
+        image: "/portfolio/project-2/img500 (1).jpg",
         year: "2025",
-        description: "Современная спальня в стиле минимализм с акцентом на функциональность и эстетику",
+        description: "Современная квартира с акцентом неоклассики",
+        tags: ["неоклассика", "современный стиль", "функциональность"],
+        featured: true,
+        gallery: [
+            "/portfolio/project-2/img500 (1).jpg",
+            "/portfolio/project-2/img490.jpg",
+            "/portfolio/project-2/img509.jpg",
+            "/portfolio/project-2/img517.jpg",
+            "/portfolio/project-2/img525.jpg",
+            "/portfolio/project-2/img533.jpg",
+            "/portfolio/project-2/img541.jpg",
+            "/portfolio/project-2/img549.jpg"
+        ]
+    },
+    {
+        id: 3,
+        title: "СОВРЕМЕННАЯ КВАРТИРА ЖК Асылым прайм",
+        category: "ЖИЛОЙ ИНТЕРЬЕР",
+        area: "85 М²",
+        image: "/portfolio/project-3/Асылым прайм_page-0005.jpg",
+        year: "2025",
+        description: "Современная квартира в стиле минимализм с акцентом на функциональность и эстетику",
         tags: ["минимализм", "современный стиль", "функциональность"],
         featured: true,
         gallery: [
-            "/portfolio/project-2/spalnya-120-1.jpg",
-            "/portfolio/project-2/spalnya-120-2.jpg",
-            "/portfolio/project-2/spalnya-120-3.jpg"
+            "/portfolio/project-3/Асылым прайм_page-0005.jpg",
+            "/portfolio/project-3/Асылым прайм_page-0002.jpg",
+            "/portfolio/project-3/Асылым прайм_page-0003.jpg",
+            "/portfolio/project-3/Асылым прайм_page-0004.jpg",
+            "/portfolio/project-3/Асылым прайм_page-0006.jpg",
+            "/portfolio/project-3/Асылым прайм_page-0007.jpg",
+            "/portfolio/project-3/Асылым прайм_page-0008.jpg",
+            "/portfolio/project-3/Асылым прайм_page-0009.jpg"
         ]
     },
+    {
+        id: 4,
+        title: "СОВРЕМЕННАЯ КВАРТИРА ЖК ЭкоПарк",
+        category: "ЖИЛОЙ ИНТЕРЬЕР",
+        area: "68 М²",
+        image: "/portfolio/project-4/ЖК ЭкоПарк_page-0003.jpg",
+        year: "2025",
+        description: "Современная квартира в стиле минимализм с акцентом на функциональность и эстетику",
+        tags: ["минимализм", "современный стиль", "функциональность"],
+        featured: true,
+        gallery: [
+            "/portfolio/project-4/ЖК ЭкоПарк_page-0003.jpg",
+            "/portfolio/project-4/ЖК ЭкоПарк_page-0001.jpg",
+            "/portfolio/project-4/ЖК ЭкоПарк_page-0002.jpg",
+            "/portfolio/project-4/ЖК ЭкоПарк_page-0004.jpg",
+            "/portfolio/project-4/ЖК ЭкоПарк_page-0005.jpg",
+            "/portfolio/project-4/ЖК ЭкоПарк_page-0006.jpg"
+        ]
+    }
 ];
 
 // Функции для работы с портфолио
